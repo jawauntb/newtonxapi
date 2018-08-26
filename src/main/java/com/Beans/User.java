0bdad69fddaf6674f185f4362aa403beb18c5039
Jawaun <jawaun.brown95@gmail.com>
2018-08-26 14:50:51 -0400

    getters and setters commit

--- src/main/java/com/Beans/User.java
+++ src/main/java/com/Beans/User.java
@@ -1,5 +1,41 @@
 package com.Beans;
 
 public class User {
+	int user_id;
+	String first_name;
+	String last_name;
+	
+	public User (String first, String last) {
+		this.first_name = first;
+		this.last_name = last;
+		
+	}
 
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+	
+	
+	
 }
